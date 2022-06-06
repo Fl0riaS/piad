f36f63c8483c0928212d5a611827923ff31a4a9a
Jakub Znyk <zj49454@zut.edu.pl>
2022-06-06 19:48:47 +0200

    added lab12

---
+++ src/lab12.py
@@ -0,0 +1,34 @@
+from audioop import avg
+from statistics import mean
+import numpy as np
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering
+from scipy import stats
+import scipy
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.metrics import accuracy_score, jaccard_score, recall_score, roc_curve
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram,linkage
+from sklearn.datasets import make_classification
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import time
+from matplotlib import rcParams
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import auc
+import sounddevice as sd
+import soundfile as sf
+
+#zadanie 4 dodatkowe
+
